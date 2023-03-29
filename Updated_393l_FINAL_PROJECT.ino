102cd241cb34645b95fdaa1109f8e6ccf3e88437
jenniferchungdc <79483819+jenniferchungdc@users.noreply.github.com>
2023-03-29 02:34:31 -0400

    Update Updated_393l_FINAL_PROJECT.ino

--- Updated_393l_FINAL_PROJECT.ino
+++ Updated_393l_FINAL_PROJECT.ino
@@ -1,22 +1,34 @@
 long randNumber;
 
-// const for interrupt pin
+// Const for interrupt pin
 const int interruptPin = 2;
 
-volatile int gamePoint = 0;
+// volatile int gamePoint = 0;
+volatile state = HIGH;
 
-// const for checking interrupt
+// Const for checking interrupt
 const int led1 = 12;
 const int led2 = 11;
 const int led3 = 10;
 const int led4 = 9;
 
-// const for mole led
+// Const for mole led
 const int mole1 = 7;
 const int mole2 = 6;
+const int mole3 = 5;
+const int mole2 = 4;
 
-// variables
+// Define state variables for the game
+bool gameRunning = false;
+bool moleActive = false; 
+
+// Variables
 int buttonValue; // stores analog value when button is pressed
+
+// Mole variables
+volatile int currentMole = -1;
+unsigned long lastMoleTime = 0;
+const int moleInterval = 2000;
 
 void setup() {
   Serial.begin(9600); // Starts serial monitor
@@ -26,20 +38,60 @@ void setup() {
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
 
-  attachInterrupt(digitalPinToInterrupt(interruptPin), buttonPress, RISING);
 
-  //startLights();
-  
+  attachInterrupt(digitalPinToInterrupt(interruptPin), buttonPress, RISING);
 }
 
 void loop() {
+  // Game setup
+  if (!gameRunning) {
+    // Game is not running, wait for button press to start
+    // Waits for all the buttons to be released so interrupt cannot occur before game starts
+    while (digitalRead(mole1) || digitalRead(mole2) || digitalRead(mole3) || digitalRead(mole4)) {}
+    gameRunning = true;
+    int score = 0;
+    //startLights();
+    Serial.println("Starting game");
+  }
+  else{
+    // Game is running
+    if(!moleActive && millis() - lastMoleTime > moleInterval) {
+      // Checks whether mole is active AND within mole interval (while LED is on)
+      int randNumber = random(2);
+      moleActive = true;
+      switch(randNumber) {
+        case 0:
+        digitalWrite(mole1, HIGH);
+        digitalWrite(mole2, LOW);
+      }
+      lastMoleTime = millis();
+    }
+  }
 
 }
 
+// Interrupt code
 void buttonPress() {
-  
+  if(gameRunning && moleActive) {
+    
+  }
 }
 
+// all the LEDs are OFF
+ void buttonsOff() {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+ }
+
+// all the LEDs are ON
+ void buttonsOn() {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+ }
 
 // Performs really cool and totally not lame light sequence at beginning of game
 void startLights() {
